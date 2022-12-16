1daf24dbaae28ef7541f0976a232d175dc3d5ab0
ldcas-uc3m <100429021@alumnos.uc3m.es>
2022-12-16 18:49:03 +0100

    part1

--- music_arduino_part1/music_arduino_part1.ino
+++ music_arduino_part1/music_arduino_part1.ino
@@ -9,15 +9,24 @@
 // COMMENT THIS LINE TO EXECUTE WITH THE PC
 #define TEST_MODE 1
 
-#define SAMPLE_TIME 250 
+#define SAMPLE_TIME 250  // ms, freq of 40000
+#define TASK_TIME 100  // ms
 #define SOUND_PIN  11
+#define LED_PIN  13
+#define BUTTON_PIN 7
 #define BUF_SIZE 256
+
+#define CLOCK_SYSTEM 16000000
 
 /**********************************************************
  *  GLOBALS
  *********************************************************/
 unsigned char buffer[BUF_SIZE];
 unsigned long timeOrig;
+bool mute;
+
+bool value_old = false;
+
 
 /**********************************************************
  * Function: play_bit
@@ -39,7 +48,13 @@ void play_bit() {
           }
        #endif
     }
-    digitalWrite(SOUND_PIN, (data & bitwise));
+    // OCR2A = data (byte)
+    if(mute){ 
+        digitalWrite(SOUND_PIN, 0);
+    } else {
+
+        digitalWrite(SOUND_PIN, (data & bitwise));
+    }
 }
 
 /**********************************************************
@@ -50,9 +65,44 @@ void setup() {
     Serial.begin(115200);
 
     pinMode(SOUND_PIN, OUTPUT);
+    pinMode(LED_PIN, OUTPUT);
+    pinMode(BUTTON_PIN, INPUT);
+
     memset(buffer, 0, BUF_SIZE);
-    timeOrig = micros();    
+
+    timeOrig = millis();
+
+    // Interrupt (Timer 1): CTC mode, compare with OCR1A, prescaling x1
+	TCCR1A = 0;
+	TCCR1B = 0;
+	TCCR1B = _BV(WGM12) | _BV(CS10);
+	TIMSK1 = _BV(OCIE1A);  // compare with R1A
+
+	OCR1A = CLOCK_SYSTEM/(1 * (1000000 / SAMPLE_TIME)) - 1;  // 3999
 }
+
+
+ISR(TIMER1_COMPA_vect) {  // handler
+    play_bit();
+}
+
+
+void task_button_n_led() {
+    bool value = digitalRead(BUTTON_PIN);
+    bool aux;
+    if (value && !value_old) {
+        noInterrupts();
+        mute = !mute;
+        aux = mute;
+
+        interrupts();
+    }
+
+    digitalWrite(LED_PIN, aux);
+
+    value_old = value;
+}
+
 
 /**********************************************************
  * Function: loop
@@ -60,10 +110,10 @@ void setup() {
 void loop() {
     unsigned long timeDiff;
 
-    play_bit();
+    task_button_n_led();
 
-    timeDiff = SAMPLE_TIME - (micros() - timeOrig);
-    timeOrig = timeOrig + SAMPLE_TIME;
+    timeDiff = TASK_TIME - (millis() - timeOrig);
+    timeOrig = timeOrig + TASK_TIME;
 
-    delayMicroseconds(timeDiff);
+    delay(timeDiff);
 }
